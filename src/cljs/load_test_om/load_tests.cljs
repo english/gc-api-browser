4f2b984ec332823220277c87c52b13dcdc201b86
Jamie English <jamienglish@gmail.com>
2015-03-02 23:18:42 +0000

    Minimise all but the first load tests

--- src/cljs/load_test_om/load_tests.cljs
+++ src/cljs/load_test_om/load_tests.cljs
@@ -1,18 +1,14 @@
 (ns load-test-om.load-tests
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [goog.events :as events]
-            [goog.json :as gjson]
-            [clojure.set :refer [rename]]
-            [load-test-om.load-test :as load-test])
-  (:import (goog.net WebSocket EventType)))
+            [load-test-om.load-test :as load-test]))
 
 (defn load-tests [load-tests owner]
   (reify
     om/IRender
     (render [_]
-      (->> (vals load-tests)
-           (sort-by :id)
-           reverse
-           (om/build-all load-test/load-test)
-           (apply dom/ul nil)))))
+      (let [ordered (->> (vals load-tests) (sort-by :id) reverse)]
+        (apply dom/ul nil
+               (map #(om/build load-test/load-test % {:init-state {:minimised? (not= % (first ordered))}
+                                                      :key :id})
+                    ordered))))))
