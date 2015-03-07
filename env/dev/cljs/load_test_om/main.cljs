5bddc96379e1dcba434793497fa668fa52bf6d49
Jamie English <jamienglish@gmail.com>
2015-03-07 12:32:00 +0000

    Update project to keep roughly inline with new chestnut template

--- env/dev/cljs/load_test_om/main.cljs
+++ env/dev/cljs/load_test_om/main.cljs
@@ -1,4 +1,4 @@
-(ns load-test-om.dev
+(ns load-test-om.main
   (:require [load-test-om.core :as core]
             [figwheel.client :as figwheel :include-macros true]
             [cljs.core.async :refer [put!]]
@@ -8,8 +8,9 @@
 
 (figwheel/watch-and-reload
   :websocket-url "ws://localhost:3449/figwheel-ws"
-  :jsload-callback (fn [] (core/main)))
+  :jsload-callback (fn []
+                     (core/main)))
 
-(weasel/connect "ws://localhost:9001" :verbose true)
+(weasel/connect "ws://localhost:9001" :verbose true :print #{:repl :console})
 
 (core/main)
--- project.clj
+++ project.clj
@@ -4,20 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :source-paths ["src/clj" "src/cljs" "target/classes"]
+  :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2657"]
+                 [org.clojure/clojurescript "0.0-2511" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [ring "1.3.2"]
-                 [compojure "1.2.0"]
+                 [ring/ring-defaults "0.1.3"]
+                 [compojure "1.3.2"]
                  [enlive "1.1.5"]
-                 [org.om/om "0.8.0"]
-                 [figwheel "0.1.4-SNAPSHOT"]
-                 [environ "1.0.0"]
-                 [com.cemerick/piggieback "0.1.3"]
-                 [weasel "0.4.0-SNAPSHOT"]
-                 [leiningen "2.5.0"]]
+                 [org.om/om "0.8.1"]
+                 [com.cemerick/piggieback "0.1.6-SNAPSHOT"]
+                 [environ "1.0.0"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-environ "1.0.0"]]
@@ -31,28 +29,37 @@
                                         :output-dir    "resources/public/js/out"
                                         :source-map    "resources/public/js/out.js.map"
                                         :preamble      ["react/react.min.js"]
-                                        :externs       ["react/externs/react.js"]
                                         :optimizations :none
                                         :pretty-print  true}}}}
 
-  :profiles {:dev {:repl-options {:init-ns load-test-om.server
+  :profiles {:dev {:source-paths ["env/dev/clj"]
+                   :test-paths ["test/clj"]
+
+                   :dependencies [[figwheel "0.2.1-SNAPSHOT"]
+                                  [figwheel-sidecar "0.2.1-SNAPSHOT"]
+                                  [com.cemerick/piggieback "0.1.3"]
+                                  [weasel "0.4.2"]]
+
+                   :repl-options {:init-ns load-test-om.server
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
-                   :plugins [[lein-figwheel "0.1.4-SNAPSHOT"]]
+                   :plugins [[lein-figwheel "0.2.1-SNAPSHOT"]]
 
                    :figwheel {:http-server-root "public"
-                              :port 3449
+                              :server-port 3449
                               :css-dirs ["resources/public/css"]}
 
                    :env {:is-dev true}
 
                    :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]}}}}
 
-             :uberjar {:hooks [leiningen.cljsbuild]
+             :uberjar {:source-paths ["env/prod/clj"]
+                       :hooks [leiningen.cljsbuild]
                        :env {:production true}
                        :omit-source true
                        :aot :all
                        :cljsbuild {:builds {:app
                                             {:source-paths ["env/prod/cljs"]
-                                             :compiler {:optimizations :advanced
-                                                        :pretty-print false}}}}}})
+                                             :compiler
+                                             {:optimizations :advanced
+                                              :pretty-print false}}}}}})
