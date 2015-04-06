4eabb130612dc2240dd6c3686945e3ec9e81e21c
Jamie English <jamienglish@gmail.com>
2015-04-06 16:05:33 +0100

    schema selection

--- src/cljs/load_test_client/form.cljs
+++ src/cljs/load_test_client/form.cljs
@@ -1,45 +1,69 @@
 (ns load-test-client.form
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [goog.events :as events])
+            [goog.json :as gjson]
+            [goog.events :as events]
+            [cljs.core.async :refer [put! chan <!]])
   (:import [goog.net XhrIo EventType]
            [goog json]))
 
-(defn handle-input-change [form e]
-  (om/transact! form [:selected-resource 0] #(.. e -target -value))
-  (om/transact! form [:selected-resource 1]
-                #(first (get (:resources form)
-                             (first (:selected-resource form))))))
+(defn actions-for-resource [schema resource]
+  (map :rel (get-in schema [:definitions (keyword resource) :links])))
+
+(defn handle-resource-change [form e]
+  (let [resource (.. e -target -value)]
+    (om/update! form :selected-resource resource)
+    (om/update! form :selected-action (first (actions-for-resource (:schema form) resource)))))
 
 (defn handle-action-change [form e]
-  (om/transact! form [:selected-resource 1] #(.. e -target -value)))
+  (om/update! form :selected-action (.. e -target -value)))
 
-(defn endpoint-selection [{:keys [url]}]
+(defn read-as-text [file c]
+  (let [reader (js/FileReader.)]
+    (set! (.-onload reader) #(put! c (.. % -target -result)))
+    (.readAsText reader file)
+    c))
+
+(defn schema->resources [schema]
+  (map name (keys (:definitions schema))))
+
+(defn handle-schema-input-change [form evt]
+  (let [file (first (array-seq (.. evt -target -files)))]
+    (go (let [text (<! (read-as-text file (chan)))
+              schema (-> text gjson/parse (js->clj :keywordize-keys true))
+              resource (first (schema->resources schema))]
+          (om/update! form :schema schema)
+          (om/update! form :selected-resource resource)
+          (om/update! form :selected-action (first (actions-for-resource schema resource)))))
+    false))
+
+(defn schema-file [form]
   (dom/div
-    #js {:className "load-test-form--field load-test-form--field__endpoint"}
-    (dom/div #js {:className "label"} "Endpoint:")
-    (dom/input #js {:type "text"
-                    :value url
+    #js {:className "load-test-form--field load-test-form--field__schema"}
+    (dom/div #js {:className "label"} "Schema:")
+    (dom/input #js {:type "file"
                     :className "input"
-                    :disabled "true"})))
+                    :accept "application/json"
+                    :onChange (partial handle-schema-input-change form)})))
 
-(defn resource-selection [{:keys [selected-resource resources] :as form}]
+(defn resource-selection [{:keys [selected-resource selected-action schema] :as form}]
   (dom/div #js {:className "load-test-form--field load-test-form--field__resource"}
            (dom/div #js {:className "label"} "Resource:")
            (apply dom/select #js {:className "input"
-                                  :value (first (:selected-resource form))
-                                  :onChange (partial handle-input-change form)}
+                                  :value selected-resource
+                                  :onChange (partial handle-resource-change form)}
                   (map #(dom/option #js {:value %} %)
-                       (keys (:resources form))))))
+                       (schema->resources schema)))))
 
-(defn action-selection [{:keys [selected-resource resources] :as form}]
+(defn action-selection [{:keys [selected-resource selected-action schema] :as form}]
   (dom/div #js {:className "load-test-form--field load-test-form--field__action"}
            (dom/div #js {:className "label"} "Action:")
            (apply dom/select #js {:className "input"
-                                  :value (second selected-resource)
+                                  :value (when selected-action (name selected-action))
                                   :onChange (partial handle-action-change form)}
                   (map #(dom/option #js {:value %} %)
-                       (get resources (first selected-resource))))))
+                       (actions-for-resource schema selected-resource)))))
 
 (defn handle-duration-change [form e]
   (om/transact! form #(assoc form :duration (.. e -target -value))))
@@ -59,16 +83,15 @@
            (dom/input #js {:className "input" :type "number" :value rate :min "1" :max "20" :step "1"
                            :onChange (partial handle-rate-change form)})))
 
-(defn handle-submit [{:keys [duration rate] :as form} {:keys [http-url] :as api}]
-  (let [[resource action] (:selected-resource form)
-        duration (js/parseInt duration)
+(defn handle-submit [{:keys [selected-resource selected-action duration rate] :as form} {:keys [http-url] :as api}]
+  (let [duration (js/parseInt duration)
         rate (js/parseInt rate)]
     (doto (XhrIo.)
       (events/listen EventType.SUCCESS #(.log js/console "SUCCESS" %))
       (events/listen EventType.ERROR #(.log js/console "ERROR" %))
       (.send (str http-url "load-tests")
              "POST"
-             (.serialize json (clj->js {:resource resource :action action :duration duration :rate rate}))
+             (.serialize json (clj->js {:resource selected-resource :action selected-action :duration duration :rate rate}))
              #js {"Content-Type" "application/json"}))))
 
 (defn submit-form [form api]
@@ -85,7 +108,7 @@
       (dom/div #js {:className "container"}
                (dom/div #js {:className "main"}
                         (dom/div #js {:className "well load-test-form"}
-                                 (endpoint-selection form)
+                                 (schema-file form)
                                  (resource-selection form)
                                  (action-selection form)
                                  (duration-selection form)
