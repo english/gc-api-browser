57221227791d74d0a98d556ef2f369cab3c42685
Jamie English <jamienglish@gmail.com>
2015-09-01 13:55:20 +0100

    Fix startsWith usage
    Looks like closure "advanced compiles" away String.prototype.startsWith. Maybe its because
    its an ES6 function and it isn't whitelisted during compilation...

--- src/cljs/gc_api_browser/schema_example.cljs
+++ src/cljs/gc_api_browser/schema_example.cljs
@@ -2,12 +2,17 @@
   (:require [clojure.string :as string])
   (:import [goog.format JsonPrettyPrinter]))
 
+(defn- starts-with? [s prefix]
+  (= 0 (.indexOf s prefix)))
+
+(defn- extract-first-json-part [example-str]
+  (string/join (->> (string/split example-str #"\n")
+                    (drop-while #(not (starts-with? % "{")))
+                    (take-while #(not (starts-with? % "H"))))))
+
 (defn prettify [schema-example-str]
-  (let [example-json-str (string/join (take-while #(not (.startsWith % "H"))
-                                                  (drop-while #(not (.startsWith % "{"))
-                                                              (string/split schema-example-str #"\n"))))]
+  (let [example-json-str (extract-first-json-part schema-example-str)]
     (try
       (.format (JsonPrettyPrinter.) example-json-str)
       (catch js/Error _
-        (js/console.error "Bad example:" schema-example-str)
         example-json-str))))
