f2a3e375f33cea5dc60914501eb665d34ab95c85
Jamie English <jamienglish@gmail.com>
2015-09-27 21:20:16 +0100

    test get-domain

---
+++ test/cljs/gc_api_browser/schema_select_test.cljs
@@ -0,0 +1,18 @@
+(ns gc-api-browser.schema-select-test
+  (:require [cljs.test :refer-macros [deftest is testing run-tests]]
+            [gc-api-browser.schema-select :refer [get-domain]]))
+
+(def schema
+  {:links [{:href "http://gocardless.com"}]})
+
+(deftest test-get-domain
+  (testing "with an existing url set"
+    (is (= "http://example.com"
+           (get-domain schema {:url "http://example.com/foobar"}))))
+  (testing "with no existing url set"
+    (is (= "http://gocardless.com"
+           (get-domain schema {:url nil})))))
+
+(comment
+  (run-tests)
+  )
